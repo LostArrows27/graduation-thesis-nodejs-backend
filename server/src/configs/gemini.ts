38d618814e25440fc3e06e5a127505fca51770fc
LostArrows27 <thelastofus2isnuts@gmail.com>
2025-03-06 10:17:19 +0700

    dep: update gemini model

--- server/src/configs/gemini.ts
+++ server/src/configs/gemini.ts
@@ -4,7 +4,8 @@ import { config } from "./setting";
 const genAI = new GoogleGenerativeAI(config.gemini_api_key);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-lite",
+  // model: "gemini-2.0-flash-lite",
+  model: "gemini-2.0-flash",
 });
 
 export default model;
